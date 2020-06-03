cabffd2bb8f9b04704037c443c01fa446128b502
Peter <pjdyson>
2020-06-03 22:18:43 +0100

    Built first version of the policy generator.

---
+++ __test_workspace__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+startdate = dt.datetime.strptime('31/12/2019', '%d/%m/%Y')
+startdate 
+
+x = list(np.random.uniform(0,365.25, 1000))
+x.sort()
+
+x = np.random.normal(size=1000)
+np.mean(x)
+np.std(x)
+
+
+
+
+
+
--- generators.py
+++ generators.py
@@ -6,24 +6,38 @@ import numpy as np
 import datetime as dt
 
 
-def generate_policy(n_policies=1000, start_date):
+def generate_policies(start_date, insured_amount, n_policies=1000):
     '''
     generates a set number of policies representing a single origin year
     '''
-    db_policy = pd.DataFrame({'Policy_ID':range(n_policies)})
-    db_policy.set_index('Policy_ID', inplace=True)
     
-    #Set policy start date (uniform dist of days from start date)
-        # no seasonality
-        
+    #generate policy start/end dates (uniformly distributed)
+    days_in_year = 365.25
+    start_date_offset = list(np.random.uniform(0,days_in_year, n_policies))
+    start_date_offset.sort()
+    start_date = [start_date + dt.timedelta(days=x) for x in start_date_offset]
+    end_date = [x + dt.timedelta(days=days_in_year) for x in start_date]
+    
     #Create risk factors (stored as standard normals)
+    policy_risk_factors_1 = np.random.normal(size=n_policies)
+    policy_risk_factors_2= np.random.normal(size=n_policies)
+    policy_risk_factors_3= np.random.normal(size=n_policies)
     
-    #Insured value (Average?)
+    # build the dataframe
+    db_policies = pd.DataFrame({'Policy_ID':range(n_policies), 
+                              'Start_date':start_date,
+                              'End_date':end_date,
+                              'Policy_risk_factor_1':policy_risk_factors_1,
+                              'Policy_risk_factor_2':policy_risk_factors_2,
+                              'Policy_risk_factor_3':policy_risk_factors_3,
+                              'Insured_amount':insured_amount})
+    db_policies.set_index('Policy_ID', inplace=True)
     
-    return db_policy 
+    return db_policies 
 
-def generate_claim(db_policy):
-    #nothing
+def generate_claims(db_policy):
+    
+    db_policy_claims = db_policy.copy()
     
     #Ultimate Claim Flag
     
@@ -33,9 +47,13 @@ def generate_claim(db_policy):
     
     #Set Paid Date
     
-    x=None
+    return db_policy_claims
     
 
+start_date = dt.datetime.strptime('01/01/2019', '%d/%m/%Y')
 
-db_policy = generate_policy(1000)
-db_policy
+db_policies = generate_policies(insured_amount=1000,
+                              n_policies=1000,
+                              start_date=start_date)
+db_policies_claims = generate_claims(db_policies)
+db_policies_claims.head()
