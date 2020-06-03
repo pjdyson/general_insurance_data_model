58287fd94429851065aec5bee47681c99e212c50
Peter <pjdyson>
2020-06-03 17:57:18 +0100

    Updated notes for todo tasks

--- generators.py
+++ generators.py
@@ -3,14 +3,23 @@
 
 import pandas as pd
 import numpy as np
+import datetime as dt
 
 
-def generate_policy(n_policies=1000):
+def generate_policy(n_policies=1000, start_date):
     '''
     generates a set number of policies representing a single origin year
     '''
     db_policy = pd.DataFrame({'Policy_ID':range(n_policies)})
     db_policy.set_index('Policy_ID', inplace=True)
+    
+    #Set policy start date (uniform dist of days from start date)
+        # no seasonality
+        
+    #Create risk factors (stored as standard normals)
+    
+    #Insured value (Average?)
+    
     return db_policy 
 
 def generate_claim(db_policy):
@@ -18,7 +27,7 @@ def generate_claim(db_policy):
     
     #Ultimate Claim Flag
     
-    #Set value
+    #Set value (% of insured value, normal dist?)
     
     #Set Reported Date
     
