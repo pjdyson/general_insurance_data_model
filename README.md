b3bc1c2e4dc40405ae1ecbe73294e14c3a0972ce
Peter <pjdyson>
2020-06-05 14:45:42 +0100

    Applied claim limit and excess and updated stats summary.

--- README.md
+++ README.md
@@ -4,3 +4,26 @@ Module to generate artificial data to test reserving engines
 ## Overview
 The intention of this module is to allow the generation of a simple set of data which represents a set of insurance policies. The data generated can then be used to share demonstrations of new actuarial processes, models and software.
 While there is really no substitute for real data, this is often commercially sensitive, so this may be the next best thing to help share ideas and solutions within the community.
+
+## Policy generation
+- Policies are assumed to be 12 months
+- Start date of policies are uniformly distributed within 12 month period (no seasonality)
+- End date of policies are 365.25 days from start date
+- Three generic risk factors for the policy are generated (standard normals) [to be used to influence claim parameters]
+- Option to set a policy excess and limit
+- Option to set policy premium
+
+## Claim generation
+- Claims are assumed to arise from a single peril
+- Only a single claim is generated per policy (or represeting total of policy claims). The policy is not terminated
+- Frequency is set as ground-up frequency, if excess frequeny is used adjust policy excess to zero and limit as the policy exposed value
+- Claim value generated from a lognormal with mean and SD
+- Timeings are calibrated using distibutions, offset as follows
+-- Incident date is set from a uniform distribution within policy year (no seasonality)
+-- Reported date is the delay in days from the incident
+-- Payment date is the delay in days from the reporting of the claim
+
+## Historic years
+- You can specify the number of historic years you would like in your data set
+- Years are offset by 365.25 days which is not quite correct (due to leap year) but used as an approximation here
+- You can specify a growth rate for the number of policies but all other parameters are assumed to be the same for prior years
--- generators.py
+++ generators.py
@@ -9,11 +9,12 @@ import datetime as dt
 days_in_year = 365.25
 
 
-def generate_policies(start_date, 
+def generate_policies(uw_start_date, 
                       insured_limit,
                       insured_excess,
                       policy_premium,
-                      n_policies=1000):
+                      n_policies,
+                      class_name):
     '''
     generates a set number of policies representing a single origin year
     '''
@@ -22,18 +23,19 @@ def generate_policies(start_date,
     days_in_year = 365.25
     start_date_offset = list(np.random.uniform(0,days_in_year, n_policies))
     start_date_offset.sort()
-    start_date = [start_date + dt.timedelta(days=x) for x in start_date_offset]
-    end_date = [x + dt.timedelta(days=days_in_year) for x in start_date]
+    uw_start_date = [uw_start_date + dt.timedelta(days=x) for x in start_date_offset]
+    uw_end_date = [x + dt.timedelta(days=days_in_year) for x in uw_start_date]
     
     #Create risk factors (stored as standard normals)
     policy_risk_factors_1 = np.random.normal(size=n_policies)
-    policy_risk_factors_2= np.random.normal(size=n_policies)
-    policy_risk_factors_3= np.random.normal(size=n_policies)
+    policy_risk_factors_2 = np.random.normal(size=n_policies)
+    policy_risk_factors_3 = np.random.normal(size=n_policies)
     
     # build the dataframe
-    db_policies = pd.DataFrame({'Policy_ID':range(n_policies), 
-                              'Start_date':start_date,
-                              'End_date':end_date,
+    db_policies = pd.DataFrame({'Class_name': class_name,
+                                'Policy_ID':range(n_policies), 
+                              'Start_date':uw_start_date,
+                              'End_date':uw_end_date,
                               'Policy_premium': policy_premium,
                               'Policy_risk_factor_1':policy_risk_factors_1,
                               'Policy_risk_factor_2':policy_risk_factors_2,
@@ -72,12 +74,18 @@ def generate_claims(db_policies,
     db_policy_claims['Claim_value_gu'] = np.where(db_policy_claims['Claim_flag']==1, 
                     np.random.lognormal(mean=mu_cvalue, sigma=sigma_cvalue, size=len(db_policy_claims)),np.nan)
     
-    #set incident date
+    #apply limit and excess to claim value
+    db_policy_claims['Claim_value'] = db_policy_claims['Claim_value_gu'] - db_policy_claims['Insured_excess']
+    db_policy_claims['Claim_value'] = db_policy_claims[['Claim_value','Insured_limit']].min(axis='columns')
+    db_policy_claims['Claim_value'] = np.where(db_policy_claims['Claim_value']<0,0,db_policy_claims['Claim_value'])
+    db_policy_claims['Claim_value'] = np.where(db_policy_claims['Claim_flag']==0,np.nan,db_policy_claims['Claim_value'])
+    
+    #set incident days
     db_policy_claims['Claim_incident_days'] = np.where(db_policy_claims['Claim_flag']==1, 
                     np.random.uniform(0,days_in_year),np.nan)
     
     
-    #Set Reported Date
+    #Set Reported days
     mu_reportdays = np.log((delay_reportdays_mean**2.0)/np.sqrt(delay_reportdays_mean**2.+delay_reportdays_sd**2.))
     sigma_reportdays = np.sqrt(np.log((delay_reportdays_sd**2.)/(delay_reportdays_sd**2.)+1))
 
@@ -85,37 +93,98 @@ def generate_claims(db_policies,
                     np.random.lognormal(mean=mu_reportdays, sigma=sigma_reportdays, size=len(db_policy_claims)),np.nan)
     
 
-    #Set Paid Date
+    #Set Paid days
     mu_paymentdays = np.log((delay_paymentdays_mean**2.0)/np.sqrt(delay_paymentdays_mean**2.+delay_paymentdays_sd**2.))
     sigma_paymentdays = np.sqrt(np.log((delay_paymentdays_sd**2.)/(delay_paymentdays_mean**2.)+1))
     
     db_policy_claims['Claim_payment_days'] = np.where(db_policy_claims['Claim_flag']==1, 
                     np.random.lognormal(mean=mu_paymentdays, sigma=sigma_paymentdays, size=len(db_policy_claims)),np.nan)
 
+    #convert days to dates
+    db_policy_claims['Claim_incident_days'] = pd.to_timedelta(db_policy_claims['Claim_incident_days'], unit='days')
+    db_policy_claims['Claim_report_days'] = pd.to_timedelta(db_policy_claims['Claim_report_days'], unit='days')
+    db_policy_claims['Claim_payment_days'] = pd.to_timedelta(db_policy_claims['Claim_payment_days'], unit='days')
 
-    cols_to_drop = ['Claim_flag_rnd']
+    db_policy_claims['Claim_incident_date'] = db_policy_claims['Start_date'] +     db_policy_claims['Claim_incident_days']
+    db_policy_claims['Claim_report_date'] = db_policy_claims['Claim_incident_date'] +     db_policy_claims['Claim_report_days']
+    db_policy_claims['Claim_payment_date'] = db_policy_claims['Claim_report_date'] +     db_policy_claims['Claim_payment_days']
+
+    db_policy_claims['End_date'] = np.where(db_policy_claims['Claim_flag']==1, 
+                    db_policy_claims['Claim_incident_date'],db_policy_claims['End_date'])
+    
+    #tidy up the dataframe
+    cols_to_drop = ['Claim_flag_rnd',
+                    'Claim_flag',
+                    'Claim_incident_days',
+                    'Claim_report_days',
+                    'Claim_payment_days']
     db_policy_claims.drop(cols_to_drop, axis='columns', inplace=True)
     
-    
     return db_policy_claims
+
+def summary_stats(db):
+    db['UW_year'] = db['Start_date'].dt.year
+    stats = db.groupby('UW_year').agg({
+            'Start_date':'count',
+            'Policy_premium':np.sum,
+            'Claim_value':np.sum
+                      })
+    stats['Loss_ratio'] = stats['Claim_value']/stats['Policy_premium']
+    stats.rename({'Start_date':'Policies_written'}, axis='columns', inplace=True)
+    return stats
     
+def generate_ultimate_portfolio(
+        class_name='Class A',
+        insured_limit=3000,
+        insured_excess=250,
+        policy_premium=150,
+        n_policies=1000,
+        uw_start_date=dt.datetime.strptime('01/01/2019', '%d/%m/%Y'),
+        historic_years=10,
+        historic_policy_growth=0.03,
+        frequency=0.15, 
+        severity_mean_gu=1000,
+        severity_sd_gu=500,
+        delay_reportdays_mean=10,
+        delay_reportdays_sd=10,
+        delay_paymentdays_mean=40,
+        delay_paymentdays_sd=10):
+
+    db_policy = []
+    
+    for year_offset in range(0,historic_years):
+        uw_start_date = uw_start_date - dt.timedelta(days=days_in_year)
+            # offsets start date back 365.25 days. This is not quite correct (due to leap years)
+        temp_policy = generate_policies(class_name=class_name,
+                                        insured_limit=insured_limit,
+                                        insured_excess=insured_excess,
+                                        policy_premium=policy_premium,
+                                        n_policies=n_policies,
+                                        uw_start_date=uw_start_date)
+        
+        temp_policy = generate_claims(db_policies=temp_policy,
+                                             frequency=frequency, 
+                                             severity_mean_gu=severity_mean_gu,
+                                             severity_sd_gu=severity_sd_gu,
+                                             delay_reportdays_mean=delay_reportdays_mean,
+                                             delay_reportdays_sd=delay_reportdays_sd,
+                                             delay_paymentdays_mean=delay_paymentdays_mean,
+                                             delay_paymentdays_sd=delay_paymentdays_sd,
+                                             )
+        
+        db_policy.append(temp_policy)
+
+        # scale down policy number with growth rate
+        n_policies = int(n_policies * (1-historic_policy_growth))
+
+    db_policy = pd.concat(db_policy)
+    
+    stats = summary_stats(db_policy)
+    
+    return db_policy, stats
 
 # run code to test
+if __name__ == '__main__':
+    uw_start_date = dt.datetime.strptime('01/01/2019', '%d/%m/%Y')
+    data, stats = generate_ultimate_portfolio()
 
-start_date = dt.datetime.strptime('01/01/2019', '%d/%m/%Y')
-
-db_policies = generate_policies(insured_limit=1000,
-                                insured_excess=250,
-                                policy_premium=150,
-                              n_policies=1000,
-                              start_date=start_date)
-db_policies_claims = generate_claims(db_policies=db_policies,
-                                     frequency=0.1, 
-                                     severity_mean_gu=300,
-                                     severity_sd_gu=500,
-                                     delay_reportdays_mean=10,
-                                     delay_reportdays_sd=10,
-                                     delay_paymentdays_mean=40,
-                                     delay_paymentdays_sd=10,
-                                     )
-db_policies_claims.head()
